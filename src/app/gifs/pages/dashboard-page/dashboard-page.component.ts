2d81528a0b949e59d4b1bc07cee74de11bba8415
Ronny Endara <rojaence@gmail.com>
2026-02-10 09:11:38 -0500

    add: General routes and layout structure

---
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [RouterModule],
+  templateUrl: './dashboard-page.component.html',
+  styles: ``
+})
+export default class DashboardPageComponent {
+
+}
---
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trending-page',
+  imports: [],
+  templateUrl: './trending-page.component.html',
+  styles: ``
+})
+export default class TrendingPageComponent {
+
+}
